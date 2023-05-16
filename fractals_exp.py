6450952cfb347c153862bba887e918ac3fd773ea
alibekshak <alibekshakiro1@gmail.com>
2023-05-16 22:17:35 +0600

    exp_iter

---
+++ fractals_exp.py
@@ -0,0 +1,116 @@
+# Аттрактор - придельный маршрут движения изменения заданой динамической системы
+
+import pygame
+import numpy as np
+import os
+import random
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+class SIF:
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+        self.T = self.create_funcs()
+
+    def create_funcs(self):
+        T = [] # список изображении 
+        for c in self.coeffs:
+            t = np.array(c[:4]).reshape(2, 2)
+            h = np.array(c[4:]) # вектор смящения
+            T.append((t, h))
+
+        return T
+
+    def create_attractor(self, surf_e, n_iter):
+        size = surf_e.get_size()
+
+        surf_res = pygame.Surface(size)
+        surf_iter = pygame.Surface(size)
+
+        surf_res.blit(surf_e, (0, 0))
+
+        size_np = np.array(size)
+        for n in range(n_iter):
+            surf_iter.fill(WHITE)
+            for t in self.T:
+                size_scale = np.int32(np.round(t[0] @ size_np))
+                offset = np.int32(np.round( t[1] * size_np)) 
+                s = pygame.transform.smoothscale(surf_res, size_scale)
+                surf_iter.blit(s, offset)
+
+            surf_res.blit(surf_iter, (0, 0))
+
+        return surf_res
+    
+    def get_random_T(self, pr):
+        p = random.random()
+        off = 0 
+        for i, pt in enumerate(pr):
+            if p < (pt+off):
+                return self.T[i]
+            off += pt
+        return False
+    
+    def get_prabobilities(self):
+        dets = [np.abs(np.linalg.det(t[0])) + 0.1 for t in self.T] # список из модулей апределителя
+        s = sum(dets)
+        return [d/s for d in dets]
+    
+    def get_next_point(self, pos, pt, scale): 
+        p = self.get_prabobilities()
+        t = self.get_random_T(p)
+
+        if not t:
+            return pt # координата текушей точки
+
+        pt_new = t[0] @pt + t[1] * scale + pos #  сперва формируется новые координвты точи, затем добовляем задоное смещение (scale - маштаб, pos - смещение на нужную позицию)
+        return (pt_new[0], pt_new[1])
+
+
+# Для появления окна
+x = 20
+y = 40
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+
+pygame.init()
+
+W = 900
+H = 600
+
+sc = pygame.display.set_mode((W, H)) 
+pygame.display.set_caption("Итеративная функция")
+sc.fill(WHITE)
+
+surf_draw = pygame.Surface((W, H))
+surf_draw.fill(WHITE)
+
+C = [(0.5, 0, 0, 0.5, 0, 0),
+    (0.5, 0, 0, 0.5, 0.5, 0),
+    (0.5, 0, 0, 0.5, 0.25, 0.433),
+]  # коэффициенты для сжимающих отображении
+
+color = (79, 42, 15)
+sif = SIF(C)
+pt = (0,0)
+scale = (400, 400)
+pos = (0, 0)
+
+FPS = 30
+clock = pygame.time.Clock()
+
+n_iter = 1
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    if n_iter < 100000:
+        pygame.draw.circle(surf_draw, color, (round(pt[0]), round(pt[1])), 1)
+        pt = sif.get_next_point(pos, pt, scale)
+        n_iter += 1
+
+        sc.blit(pygame.transform.flip(surf_draw, False, True), (200, -100)) # отбражение на главной плоскати
+
+    pygame.display.update()
+    clock.tick(FPS)
